7292949bfa6b3faff1ef2330a2a66fd41da25f0d
abhilashdk2016 <abhilashdk2014@outlook.com>
2025-01-25 23:54:56 +0530

    Client App -  Authentication Completed

---
+++ uptimer-client/src/utils/utils.ts
@@ -0,0 +1,27 @@
+import { toast } from "react-toastify";
+
+export const successToast = (message: string): void => {
+    toast.success(message, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored'
+    });
+}
+
+export const errorToast = (message: string): void => {
+    toast.error(message, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored'
+    });
+}
